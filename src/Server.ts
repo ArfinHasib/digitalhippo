f7d94d537ee2d7eb00c5baa4728d7c158f96ed6c
ArfinHasib <arfinhasib21375@gmail.com>
2024-01-06 23:29:58 +0600

    Setting up the server

--- src/Server.ts
+++ src/Server.ts
@@ -1,10 +1,31 @@
 import express from 'express';
+import { getPayloadClient } from './get-payload';
+import { nextApp, nextHandler } from './next-utils';
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
 
 const start = async () => {
-   const paylod = await getPayloadClient();
+   const paylod = await getPayloadClient({
+      initOptions: {
+         express: app,
+         onInit: async (cms) => {
+            cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+         },
+      },
+   });
+
+   app.use((req, res) => nextHandler(req, res));
+
+   nextApp.prepare().then(() => {
+      paylod.logger.info('Next.js started');
+
+      app.listen(port, async () => {
+         paylod.logger.info(
+            `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+         );
+      });
+   });
 };
 
 start();
